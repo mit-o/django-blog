bb2c8a7a063eeea8e7e3639ca2849cb0f844f00e
Sebastian Tarczynski <starczynski7@gmail.com>
2021-02-26 04:05:02 +0100

    model for like feature

--- blog/models.py
+++ blog/models.py
@@ -7,11 +7,29 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes= models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    liked= models.ManyToManyField(User, default=None, blank=True, related_name='liked')
 
     def __str__(self):
         return self.title
     
     def  get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+    @property
+    def num_likes(self):
+        return self.liked.all().count()
+
+
+LIKE_CHOICES = (
+    ('Like', 'Like'),
+    ('Unlike', 'Unlike'),
+)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    value = models.CharField(choices=LIKE_CHOICES, default='Like', max_length=10)
+
+    def __str__(self):
+        return str(self.post)
