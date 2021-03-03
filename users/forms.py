62113be0ab682f0df0f76c95262349d983193102
Sebastian Tarczynski <starczynski7@gmail.com>
2021-03-03 11:48:01 +0100

    add profile img size limit

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
+from django.core.exceptions import ValidationError
+
+def file_size(value):
+    limit = 2 * 1024 * 1024 # 2 MiB
+    if value.size > limit:
+        raise ValidationError('File too large. Size should not exceed 2 MB.')
+
+
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
@@ -19,6 +27,8 @@ class UserUpdateForm(forms.ModelForm):
         fields = ['username', 'email']
 
 class ProfileUpdateForm(forms.ModelForm):
+    image = forms.FileField(validators=[file_size])
+
     class Meta:
         model = Profile
         fields = ['image']
